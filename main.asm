2343f9269d29101705a86cbb0885cb3d9d3015d4
Sam Bonnekamp <sam@bonnekamp.net>
2026-01-04 19:32:59 +0800

    added delay routine

--- main.asm
+++ main.asm
@@ -5,67 +5,68 @@
 .DEFINE LOOP_MI 0x69
 .DEFINE LOOP_HI 0x18
 .CSEG
-.org 0
-        jmp     _start
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
-        jmp     __bad_interrupt
+.org 0                          ;vector jump table
+        jmp _start
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
+        jmp __bad_interrupt
 
 _start:
-        eor     r1, r1
-        out     0x3f, r1        ;zero SREG
-        ldi     r28, 0xFF
-        ldi     r29, 0x08
-        out     0x3e, r29       ;init SPH (stack pointer high) (i think)
-        out     0x3d, r28       ;init SPL (stack pointer low)
-        call    main
-        jmp     _exit
+        eor r1, r1
+        out 0x3f, r1        ;zero SREG
+        ldi r28, 0xFF
+        ldi r29, 0x08
+        out 0x3e, r29       ;init SPH (stack pointer high) (i think)
+        out 0x3d, r28       ;init SPL (stack pointer low)
+        call main
+        jmp _exit
 
 main:
-        ldi     r24, 0x02
-        out     DDRB, r24       ;r24 = 2 -> set pin PB1 to output
+        ldi r24, 0x02
+        out DDRB, r24       ;r24 = 2 -> set pin PB1 to output
 while:
-        out     PORTB, r1       ;r1 = 0 -> all pins to low
-        ldi     r18, LOOP_LO
-        ldi     r19, LOOP_MI
-        ldi     r25, LOOP_HI
-off_loop:
-        subi    r18, 0x01
-        sbci    r19, 0x00
-        sbci    r25, 0x00
-        brne    off_loop
-        out     PORTB, r24      ;r24 = 2 -> PB1 to high
-        ldi     r18, LOOP_LO
-        ldi     r19, LOOP_MI
-        ldi     r25, LOOP_HI
-on_loop:
-        subi    r18, 0x01
-        sbci    r19, 0x00
-        sbci    r25, 0x00
-        brne    on_loop
-        rjmp    while
+        out PORTB, r1           ;r1 = 0 -> all pins to low
+        call delay
+        out PORTB, r24          ;r24 = 2 -> PB1 to high
+        call delay
+        jmp while
+
+
+
+
+delay:                          ;a busy loop lol
+        ldi r18, LOOP_LO
+        ldi r19, LOOP_MI
+        ldi r25, LOOP_HI
+subbing:
+        subi r18, 1
+        sbci r19, 0
+        sbci r25, 0
+        brne subbing
+        ret
+
 
 _exit:
         cli
